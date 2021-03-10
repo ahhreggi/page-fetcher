b0e8959fb1f3793f259433cfce3a036dc115ce5a
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-10 22:26:29 +0000

    Implement writeToFile

--- fetcher.js
+++ fetcher.js
@@ -4,28 +4,44 @@
 //   Downloaded and saved 3261 bytes to ./index.html
 
 const fs = require('fs');
+const readline = require('readline');
 
-const url = process.argv[2];
-const path = process.argv[3];
+const url = process.argv[2] || '';
+const path = process.argv[3] || '';
 
 const request = require('request');
-request(url, (error, response, body) => {
-  if (error) {
-    console.log("Invalid URL");
-  } else {
-    if (!fs.existsSync(path)) {
-      // Write to file
-      writeToFile(path, body);
-    } else {
-      console.log("File already exists!");
-    }
-  }
-});
 
 const writeToFile = function(path, body) {
   fs.writeFile(path, body, function(err) {
-    if (err) throw err;
-    const size = fs.statSync(path).size;
-    console.log(`Downloaded and saved ${size} bytes to ${path}`);
+    // If an error occurs during file write, log an error message
+    if (err) {
+      console.log("An error occurred! Please ensure you have entered a valid URL and local file path.");
+    // If the file write is successful, log a confirmation message
+    } else {
+      const size = fs.statSync(path).size;
+      console.log(`Downloaded and saved ${size} bytes to ${path}`);
+    }
   });
 };
+
+request(url, (error, response, body) => {
+  // If the file doesn't exist, write to file
+  if (!fs.existsSync(path)) {
+    // Write to file
+    writeToFile(path, body);
+  // If the file already exists, ask to overwrite
+  } else {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    rl.question(`The specified path already exists. Overwrite? (Y/N): `, (ans) => {
+      if (ans.toLowerCase() === 'y') {
+        writeToFile(path, body);
+      } else {
+        console.log("Exiting app...");
+      }
+      rl.close();
+    });
+  }
+});
